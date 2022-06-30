340163454da222678afa5a30d841f37432b2f132
alexandre$ <108496710+alexandre-03@users.noreply.github.com>
2022-06-30 20:56:05 -0300

    Add files via upload
    envio introdução 30-06-22

---
+++ kaua-ipw-4/3_array/1_array.php
@@ -0,0 +1,47 @@
+<?php
+$cores1 = array('vermelho','azul', 'verde', 'amarelo');
+
+  
+ var_dump($cores1);
+ echo '<br>';
+ echo $cores1[0];
+ echo '<br>';
+ echo $cores1[1];
+ echo '<br>';
+ echo $cores1[2];
+ echo '<br>';
+ echo $cores1[3];
+ echo '<br>';
+ 
+          
+	
+  $frutas = array(0='Morango', 69=>'laranja', 2=>'kiwi');
+ 
+    
+	 var_dump($frutas);
+	 echo'<br>';
+	 echo$frutas[0];
+	 echo'<br>';
+	 echo$frutas[69];
+	 echo'<br>';
+	 echo$frutas[2];
+	 echo'<br>';
+	   echo'<hr>';
+	   
+   
+   
+   
+   echo '<br>';
+   $carros = [0=>'parati',1=>'ranger',2=>'golf',3=>'cruze'];
+    
+	var_dump($carros);
+	echo'<br>';
+	echo $carros[0];
+    echo'<br>';
+	echo $carros[1];
+    echo'<br>';
+	echo $carros[2];
+    echo'<br>';
+	echo $carros[3];
+    echo'<br>';
+>?
